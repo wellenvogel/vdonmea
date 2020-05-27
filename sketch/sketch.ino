e070c8f8c3421ee71ebb7278992ae8559e1dbddd
andreas <andreas@wellenvogel.de>
2020-05-27 21:50:02 +0200

    1st working version

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,8 +4,16 @@
  * License: MIT
  */
 
+
 #define IPIN 2
+//pins - gry: vref (~8V), green,yellow: direction
+#define GREY_PIN A2
+#define YELLOW_PIN A1
+#define GREEN_PIN  A0
 #define TALKER_ID "GP"
+#define SPEED 19200
+#define INTERVAL 500
+#define SHOW_TEXT 1
 volatile unsigned long icount;
 unsigned long lastCount;
 unsigned long ts;
@@ -21,12 +29,26 @@ float hzToKn(float hz){
   return hz*HZTOKN;
 }
 
+#define R1 4.7
+#define R2 4.7
+
+#define RGREY1 6.6
+#define RGREY2 3.3
+#define ANALOG_RESOLUTION_BITS 10
+//expected voltage at grey pin - relate all measures against this
+#define VREF 8.23
+float analogResolution = 1 << ANALOG_RESOLUTION_BITS;
+float factor=(R1+R2)/R2/analogResolution;
+float factorGrey=(RGREY1+RGREY2)/RGREY2/analogResolution;
+
+
+
 void setup() {
   icount=0;
   lastCount=0;
   // put your setup code here, to run once:
   attachInterrupt(digitalPinToInterrupt(IPIN),isr,RISING);
-  Serial.begin(4800);
+  Serial.begin(SPEED);
   ts=millis();
 }
 
@@ -50,10 +72,57 @@ const char * formatNmea(float windKn,float angle){
   return nmeabuf;
 }
 
+/**
+ * computation of angles
+ * we start with some min/max values - but the will adapt
+ * yellow is sin, green is cos
+ * from the voltages we compute an angle and afterwards we average
+ */
+#define INITIAL_MIN 2.12
+#define INITIAL_MAX 6.0 
+//will be added to final result
+#define FINAL_OFFSET -5.0 
 
+
+#define ALLOWED_CHANGE 0.3
+#define CHG_MA 0.1
+
+float minValue=INITIAL_MIN;
+float maxValue=INITIAL_MAX;
+float amp=(maxValue-minValue)/2*1.002;
+
+float handleMinMax(float v){
+  bool hasChanged=false;
+  if (v > maxValue){
+    if (maxValue >= (INITIAL_MAX + ALLOWED_CHANGE) || v > (INITIAL_MAX + ALLOWED_CHANGE) ){
+      v=maxValue;
+    }
+    else{
+      maxValue=maxValue+CHG_MA*(v-maxValue);
+      hasChanged=true;
+      if (v>maxValue) v=maxValue;
+    }
+  }
+  if (v < minValue){
+    if (minValue <= (INITIAL_MIN - ALLOWED_CHANGE) || v < (INITIAL_MIN - ALLOWED_CHANGE)){
+      v=minValue;
+    }
+    else{
+      minValue=minValue - CHG_MA*(minValue-v);
+      hasChanged=true;
+      if (v < minValue) v=minValue;
+    }
+  }
+  if (hasChanged){
+    amp=(maxValue-minValue)/2*1.002;
+  }
+  return v;
+}
+
+#define MAXVAL 0.99999
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1000);
+  delay(INTERVAL);
   unsigned long currentTime=millis();
   if (currentTime <= ts){
     //overflow, skip this
@@ -72,14 +141,83 @@ void loop() {
   lastCount=currentCount;
   float freq=(float)countDiff*1000/(float)diff;
   float windKn=hzToKn(freq);
-  Serial.print("tdiff=");
-  Serial.print(diff);
-  Serial.print(", freq=");
-  Serial.print(freq);
-  Serial.print(", kn=");
-  Serial.print(windKn);
-  Serial.println();
-  Serial.print(formatNmea(windKn,0));
+  float grey=(float)analogRead(GREY_PIN)*factorGrey;
+  float windAngle=0;
+  float scale=1.0;
+  if (grey < 0.01){
+    Serial.print("grey to small");
+    Serial.println();
+  }
+  else{
+    scale=VREF/grey;
+  }
+  int green=analogRead(GREEN_PIN);
+  float vgreen=factor*green*scale;
+  int yellow=analogRead(YELLOW_PIN);
+  float vyellow=factor*yellow*scale;
+
+  float vyellowC=handleMinMax(vyellow);
+  float vgreenC=handleMinMax(vgreen);
+  float vgscaled=(vgreenC-minValue)/amp-1;
+  if (vgscaled >= 1) vgscaled=MAXVAL;
+  if (vgscaled <= -1) vgscaled=-MAXVAL;
+  float vyscaled=(vyellowC-minValue)/amp-1;
+  if (vyscaled >= 1) vyscaled=MAXVAL;
+  if (vyscaled <= -1) vyscaled=-MAXVAL;
+  //now the quadrants 
+  //asin from -90...90
+  //acos from 0...180
+  float wsin=180/PI*asin(vyscaled);
+  float wcos=180/PI*acos(vgscaled);
+  //1. 0...90 wsin ok, wcos ok (both 0...90)
+  if (vyscaled >=0 && vgscaled >=0){
+    if (wcos >= 90) wcos=180-wcos;
+  } else
+  //2. 90...180 wcos ok, wsin correct 
+  if (vyscaled >=0 && vgscaled <=0) {
+    if (wsin < 90) wsin=180.0-wsin;
+  } else
+  //3. -180...-90
+  if (vyscaled < 0 && vgscaled < 0){
+     wsin=-180-wsin;
+     wcos=-wcos;
+  }else
+  //4. -90...0 wsin ok, wcos correct
+  if (vyscaled <0 && vgscaled >=0){
+    wcos=-wcos;
+  }
+  float wfinal=(wsin+wcos)/2 + (FINAL_OFFSET);
+  
+  if (SHOW_TEXT){
+    Serial.print("tdiff=");
+    Serial.print(diff);
+    Serial.print(", freq=");
+    Serial.print(freq);
+    Serial.print(", kn=");
+    Serial.print(windKn);
+    Serial.print(", greyBase=");
+    Serial.print(grey);
+    Serial.print(", scale=");
+    Serial.print(scale);
+    Serial.print(", yellow=");
+    Serial.print(vyellow);
+    Serial.print(", green=");
+    Serial.print(vgreen);
+    Serial.print(" ,");
+    Serial.print(green);
+    Serial.print(", min=");
+    Serial.print(minValue);
+    Serial.print(", max=");
+    Serial.print(maxValue);
+    Serial.print(", amp=");
+    Serial.print(amp);
+    Serial.print(", asin=");
+    Serial.print(wsin);
+    Serial.print(", acos=");
+    Serial.print(wcos);
+    Serial.println();
+  }
+  Serial.print(formatNmea(windKn,wfinal));
   Serial.println();
   
 }
